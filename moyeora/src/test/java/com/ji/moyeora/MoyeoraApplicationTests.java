d6eada81a7e4e6e9d36f2b6723f95df78a6c7e79
wltmddk <wltmddk@neoplus.co.kr>
2025-02-19 15:27:55 +0900

    02월 19일 커밋테스트

---
+++ moyeora/src/test/java/com/ji/moyeora/MoyeoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ji.moyeora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyeoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
